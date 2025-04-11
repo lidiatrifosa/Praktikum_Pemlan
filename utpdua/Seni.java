b4b5aa03fa82776d362284da725867e6af6adcc3
lidiatrifosa <liditrifo@gmail.com>
2025-04-11 21:16:09 +0700

    Add files via upload

---
+++ utpdua/Seni.java
@@ -0,0 +1,16 @@
+package utpdua;
+
+public class Seni extends BarangLelang {
+    private String seniman;
+
+    public Seni(String namaBarang, double hargaAwal, String seniman) {
+        super(namaBarang, hargaAwal);
+        this.seniman = seniman;
+    }
+
+    public void infoSeni() {
+        System.out.println("Karya seni ini dibuat oleh: " + seniman);
+    }
+}
+
+
